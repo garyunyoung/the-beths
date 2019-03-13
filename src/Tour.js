aed207f34d187dd39142d3009fe0bf10a388bbd0
Garyun Young <garyunyoung@gmail.com>
2019-03-13 22:41:20 +0000

    Add songkick widget

--- src/Tour.js
+++ src/Tour.js
@@ -1,24 +1,41 @@
 import React from "react";
 import "./Tour.css";
 import tourVid from "./images/tour-vid-e.mp4";
-// import gif from "./images/gif.gif";
 
-export default function Tour() {
-  return (
-    <section className="tour__section">
-      <h1 className="tour__header">tour</h1>
-      <div className="tour__video"> 
-        <video
-          className="tour__video--live"
-          type="video/mp4"
-          width="100%"
-          src={tourVid}
-          autoplay="autoplay"
-          loop
-          muted
-        />
-        {/* <img className="gif" src={gif}/> */}
-      </div>
-    </section>
-  );
+export default class Tour extends React.Component {
+  componentDidMount() {
+    const script = document.createElement('script');
+    script.src = "//widget.songkick.com/8923484/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  render() {
+    return (
+      <section className="tour__section">
+        <h1 className="tour__header">tour</h1>
+        <div className="tour__video">
+          <video
+            className="tour__video--live"
+            type="video/mp4"
+            width="100%"
+            src={tourVid}
+            // autoPlay
+            loop
+            muted
+          />
+        </div>
+        <div className="song-kick">
+          <a
+            href="https://www.songkick.com/artists/8923484"
+            className="songkick-widget"
+            data-theme="light"
+            data-track-button="on"
+            data-detect-style="true"
+            data-background-color="transparent"
+          />
+        </div>
+      </section>
+    );
+  }
 }
