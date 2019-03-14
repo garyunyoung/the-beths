f1d0e7196c38eff6ccfb87e75ea42a919ef28159
Garyun Young <garyunyoung@gmail.com>
2019-03-14 12:43:23 +0000

    Fix watch thumbnail spacing

--- src/Watch.js
+++ src/Watch.js
@@ -13,20 +13,30 @@ export default function Watch() {
   return (
     <section className="watch">
       <h1 className="watch__header">watch</h1>
-      <section className="watch__subsection">
-        <h2 className="watch__title">future me hates me</h2>
+      <h2 className="watch__title">future me hates me</h2>
+      <section className="watch__wrapper">
         <Thumbnail title="uptpwn girl" image={ld} modifyer="utg" />
-        <Thumbnail title="happy unhappy" image={huh} modifier="huh"/>
-        <Thumbnail title="little death" image={ld} modifier="ld"/>
-        <Thumbnail title="great no one" image={gno} modifier="gno"/>
-        <Thumbnail title="future me hates me" image={fmhm} modifier="fmhm"/>
-        <Thumbnail title="you wouldn't like me" image={ywlm} modifier="ywlm"/>
-        <Thumbnail title="whatever" image={w} modifier="w"/>
-        <Thumbnail title="lying in the sun" image={lits} modifier="lits"/>
-        <Thumbnail title="have yourself a merry litte christmas" image={hyamlc} modifier="hyamlc"/>
+        <Thumbnail title="happy unhappy" image={huh} modifier="huh" />
+        <Thumbnail title="little death" image={ld} modifier="ld" />
+        <Thumbnail title="great no one" image={gno} modifier="gno" />
+        <Thumbnail title="future me hates me" image={fmhm} modifier="fmhm" />
+        <Thumbnail title="you wouldn't like me" image={ywlm} modifier="ywlm" />
+      </section>
+      <h2 className="watch__title">whatever</h2>
+      <section className="watch__wrapper">
+        <Thumbnail title="whatever" image={w} modifier="w" />
+        <Thumbnail title="lying in the sun" image={lits} modifier="lits" />
+      </section>
+      <h2 className="watch__title">bonus</h2>
+      <section className="watch__wrapper">
+        <Thumbnail
+          title="have yourself a merry litte christmas"
+          image={hyamlc}
+          modifier="hyamlc"
+        />
       </section>
     </section>
-  )
+  );
 }
 
 function Thumbnail(props) {
@@ -41,4 +51,3 @@ function Thumbnail(props) {
     </div>
   );
 }
-
