1418e4420ab79725039f05494709c77e10459044
Garyun Young <garyunyoung@gmail.com>
2019-03-14 11:19:18 +0000

    Add watch icons

--- src/Watch.js
+++ src/Watch.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./Watch.css";
-import lilBeth from "./images/lil-beth.jpg";
+import ld from "./images/ld.jpg";
+import huh from "./images/huh.jpg";
+import hyamlc from "./images/hyamlc.jpg";
+import lits from "./images/lits.jpg";
+import ywlm from "./images/ywlm.jpg";
+import fmhm from "./images/fmhm.jpg";
+import gno from "./images/gno.jpg";
+import w from "./images/w.jpg";
+
 
 export default function Watch() {
   //   const images = [merch1, merch1, merch1, merch1];
@@ -9,39 +17,39 @@ export default function Watch() {
       <h1 className="watch__header">watch</h1>
       <section className="watch__subsection">
         <div className="ld">
-          <img className="ld__beth" src={lilBeth} />
+          <img className="ld__beth" src={ld} />
           <p className="heading heading--ld">little death</p>
         </div>
         <div className="fmhm">
-          <img className="fmhm__beth" src={lilBeth} />
+          <img className="fmhm__beth" src={fmhm} />
           <p className="heading heading--fmhm">future me hates me</p>
         </div>
         <div className="utg">
-          <img className="utg__beth" src={lilBeth} />
+          <img className="utg__beth" src={ld} />
           <p className="heading heading--ug">uptown girl</p>
         </div>
         <div className="huh">
-          <img className="huh__beth" src={lilBeth} />
+          <img className="huh__beth" src={huh} />
           <p className="heading heading--ug">happy unhappy</p>
         </div>
         <div className="ywlm">
-          <img className="ywlm__beth" src={lilBeth} />
+          <img className="ywlm__beth" src={ywlm} />
           <p className="heading heading--ug">you wouldn't like me</p>
         </div>
         <div className="hyamlc">
-          <img className="hyamlc__beth" src={lilBeth} />
+          <img className="hyamlc__beth" src={hyamlc} />
           <p className="heading heading--ug">have yourself a <br></br>merry little christmas</p>
         </div>
         <div className="gno">
-          <img className="gno__beth" src={lilBeth} />
+          <img className="gno__beth" src={gno} />
           <p className="heading heading--ug">great noone</p>
         </div>
         <div className="w">
-          <img className="w__beth" src={lilBeth} />
+          <img className="w__beth" src={w} />
           <p className="heading heading--ug">whatever</p>
         </div>
         <div className="lits">
-          <img className="lits__beth" src={lilBeth} />
+          <img className="lits__beth" src={lits} />
           <p className="heading heading--ug">lying in the sun</p>
         </div>
       </section>
