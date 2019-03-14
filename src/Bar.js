f015033592485f6f6fbd9a696914535afb5d1b02
Garyun Young <garyunyoung@gmail.com>
2019-03-14 23:08:06 +0000

    Fix nav bar bug

--- src/Bar.js
+++ src/Bar.js
@@ -4,10 +4,28 @@ import "./Bar.css";
 import logoBlack from "./images/logo-b.jpg";
 
 export default function Bar() {
+  const openNav = () => {
+    document.querySelector(".nav__list").classList.toggle("nav__list--open");
+    let navIcon = document.querySelector(".bar__nav-icon");
+    navIcon.textContent =
+      navIcon.textContent === navIcon.dataset.textSwap
+        ? navIcon.dataset.textOriginal
+        : navIcon.dataset.textSwap;
+  };
   return (
-    <div className="bar">
-      <img className="bar__logo" src={logoBlack} />
+    <React.Fragment>
+      <div className="bar">
+        <img className="bar__logo" src={logoBlack} />
+        <p
+          className="bar__nav-icon"
+          onClick={openNav}
+          data-text-swap="x"
+          data-text-original="="
+        >
+          =
+        </p>
+      </div>
       <Nav />
-    </div>
+    </React.Fragment>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -2,18 +2,9 @@ import React from "react";
 import "./Nav.css";
 
 export default function Nav() {
-  const openNav = () => {
-    document.querySelector('.nav__list').classList.toggle('nav__list--open');
-    let navIcon = document.querySelector('.nav__icon');
-        navIcon.textContent = navIcon.textContent === navIcon.dataset.textSwap ?
-          navIcon.dataset.textOriginal : 
-          navIcon.dataset.textSwap;
-    };
+
   return (
     <div className="nav">
-      <p className="nav__icon" onClick={openNav} data-text-swap="x" data-text-original="=">
-        =
-      </p>
         <ul className="nav__list">
           <li className="nav__tour">tour</li>
           <li className="nav__merch">merch</li>
