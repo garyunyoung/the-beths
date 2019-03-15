3eb5b214d7ae9f9c88f5a29414e2b00aa43c1293
Garyun Young <garyunyoung@gmail.com>
2019-03-15 13:25:04 +0000

    Add smooth scroll in index.html

--- src/Header.js
+++ src/Header.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./Header.css";
 
 export default function Header(props) {
-    return <h1 className="heading">{props.heading}</h1>
+    return <h1 className="heading"><a name={props.heading}>{props.heading}</a></h1>
 }
--- src/Nav.js
+++ src/Nav.js
@@ -6,10 +6,10 @@ export default function Nav() {
   return (
     <div className="nav">
         <ul className="nav__list">
-          <li className="nav__tour">tour</li>
-          <li className="nav__merch">merch</li>
-          <li className="nav__watch">watch</li>
-          <li className="nav__contact">contact</li>
+          <li className="nav__item nav__item--tour"><a className="nav__link" href="#tour">tour</a></li>
+          <li className="nav__item nav__item--merch"><a className="nav__link" href="#merch">merch</a></li>
+          <li className="nav__item nav__item--watch"><a className="nav__link" href="#watch">watch</a></li>
+          <li className="nav__item nav__item--contact"><a className="nav__link" href="#contact">contact</a></li>
         </ul>
     </div>
   );
