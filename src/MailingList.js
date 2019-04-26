896bc2a1a82e255e9bca1108a564e0c2de3c10c3
Garyun Young <garyunyoung@gmail.com>
2019-04-26 15:28:15 +0100

    Modify mailing list css to sit on the left side of the landing page

--- src/MailingList.js
+++ src/MailingList.js
@@ -2,63 +2,65 @@ import React from "react";
 import dag from "./images/fmhm-dog.png";
 import "./MailingList.scss";
 
-export default function MailingList() {
-  return (
-    <section className="mailingList">
-      <img className="mailingList__dag" src={dag} />
-      {/* <div className="mailingList__wrapper">
-        <label for="email">join the mailing list!</label>
-        <input
-          className="mailingList__input"
-          type="text"
-          name="email"
-          required
-        />
-      </div> */}
+export default class MailingList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailValue: ""
+    };
+  }
 
-      <div id="mc_embed_signup">
-        <form
-          action="https://gmail.us20.list-manage.com/subscribe/post?u=0bfa27c5341eacbfd10d85a43&amp;id=05cac296b0"
-          method="post"
-          id="mc-embedded-subscribe-form"
-          name="mc-embedded-subscribe-form"
-          className="validate"
-          target="_blank"
-          novalidate
-        >
-          <div id="mc_embed_signup_scroll">
-            <label htmlFor="mce-EMAIL">join the mailing list!</label>
-            <div className="input-wrapper">
-              <input
-                type="email"
-                value=""
-                name="EMAIL"
-                className="email"
-                id="mce-EMAIL"
-                placeholder="email address"
-                required
-              />
-              <div className="aria" aria-hidden="true">
+  render() {
+    return (
+      <section className="mailingList">
+        <img className="mailingList__dag" src={dag} />
+        <div id="mc_embed_signup">
+          <form
+            action="https://gmail.us20.list-manage.com/subscribe/post?u=0bfa27c5341eacbfd10d85a43&amp;id=05cac296b0"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            className="validate"
+            target="_blank"
+            novalidate
+          >
+            <div id="mc_embed_signup_scroll">
+              <label htmlFor="mce-EMAIL">join the mailing list!</label>
+              <div className="input-wrapper">
                 <input
-                  type="text"
-                  name="b_0bfa27c5341eacbfd10d85a43_05cac296b0"
-                  tabindex="-1"
-                  value=""
+                  type="email"
+                  value={this.state.emailValue}
+                  onChange={e => {
+                    this.setState({ emailValue: e.target.value });
+                  }}
+                  name="EMAIL"
+                  className="email"
+                  id="mce-EMAIL"
+                  placeholder="email address"
+                  required
                 />
-              </div>
-              <div className="clear">
-                <input
-                  className="button"
-                  type="submit"
-                  value=">"
-                  name="subscribe"
-                  id="mc-embedded-subscribe"
-                />
+                <div className="aria" aria-hidden="true">
+                  <input
+                    type="text"
+                    name="b_0bfa27c5341eacbfd10d85a43_05cac296b0"
+                    tabindex="-1"
+                    value=""
+                  />
+                </div>
+                <div className="clear">
+                  <input
+                    className="button"
+                    type="submit"
+                    value=">"
+                    name="subscribe"
+                    id="mc-embedded-subscribe"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        </form>
-      </div>
-    </section>
-  );
+          </form>
+        </div>
+      </section>
+    );
+  }
 }
