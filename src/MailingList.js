ef06ee657725961c40e245b5a571b60bea5476d1
Garyun Young <garyunyoung@gmail.com>
2019-05-01 12:33:25 +0100

    Fix mailing list position on desktop

--- src/MailingList.js
+++ src/MailingList.js
@@ -14,52 +14,54 @@ export default class MailingList extends React.Component {
     return (
       <section className="mailingList">
         <img className="mailingList__dag" src={dag} />
-        <div id="mc_embed_signup">
-          <form
-            action="https://gmail.us20.list-manage.com/subscribe/post?u=0bfa27c5341eacbfd10d85a43&amp;id=05cac296b0"
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            className="validate"
-            target="_blank"
-            noValidate
-          >
-            <div id="mc_embed_signup_scroll">
-              <label htmlFor="mce-EMAIL">join the mailing list!</label>
-              <div className="input-wrapper">
-                <input
-                  type="email"
-                  value={this.state.emailValue}
-                  onChange={e => {
-                    this.setState({ emailValue: e.target.value });
-                  }}
-                  name="EMAIL"
-                  className="email"
-                  id="mce-EMAIL"
-                  placeholder="email address"
-                  required
-                />
-                <div className="aria" aria-hidden="true">
+        <div className="mailingList__wrapper">
+          <div id="mc_embed_signup">
+            <form
+              action="https://gmail.us20.list-manage.com/subscribe/post?u=0bfa27c5341eacbfd10d85a43&amp;id=05cac296b0"
+              method="post"
+              id="mc-embedded-subscribe-form"
+              name="mc-embedded-subscribe-form"
+              className="validate"
+              target="_blank"
+              noValidate
+            >
+              <div id="mc_embed_signup_scroll">
+                <label htmlFor="mce-EMAIL">join the mailing list!</label>
+                <div className="input-wrapper">
                   <input
-                    type="text"
-                    name="b_0bfa27c5341eacbfd10d85a43_05cac296b0"
-                    tabIndex="-1"
-                    value=""
-                    readOnly
+                    type="email"
+                    value={this.state.emailValue}
+                    onChange={e => {
+                      this.setState({ emailValue: e.target.value });
+                    }}
+                    name="EMAIL"
+                    className="email"
+                    id="mce-EMAIL"
+                    placeholder="email address"
+                    required
                   />
-                </div>
-                <div className="clear">
-                  <input
-                    className="button"
-                    type="submit"
-                    value=">"
-                    name="subscribe"
-                    id="mc-embedded-subscribe"
-                  />
+                  <div className="aria" aria-hidden="true">
+                    <input
+                      type="text"
+                      name="b_0bfa27c5341eacbfd10d85a43_05cac296b0"
+                      tabIndex="-1"
+                      value=""
+                      readOnly
+                    />
+                  </div>
+                  <div className="clear">
+                    <input
+                      className="button"
+                      type="submit"
+                      value=">"
+                      name="subscribe"
+                      id="mc-embedded-subscribe"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </section>
     );
