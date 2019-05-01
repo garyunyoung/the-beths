ee0bb00057b184c38e9ebd413e672e042893cc5f
Garyun Young <garyunyoung@gmail.com>
2019-05-01 13:21:47 +0100

    Fiz nav bac z-index

--- src/MailingList.js
+++ src/MailingList.js
@@ -35,7 +35,7 @@ export default class MailingList extends React.Component {
                       this.setState({ emailValue: e.target.value });
                     }}
                     name="EMAIL"
-                    className="email"
+                    class="email"
                     id="mce-EMAIL"
                     placeholder="email address"
                     required
@@ -67,3 +67,24 @@ export default class MailingList extends React.Component {
     );
   }
 }
+
+// <!-- Begin Mailchimp Signup Form -->
+// <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+// <style type="text/css">
+// 	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+// 	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+// 	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+// </style>
+// <div id="mc_embed_signup">
+// <form action="https://thebeths.us20.list-manage.com/subscribe/post?u=0bfa27c5341eacbfd10d85a43&amp;id=05cac296b0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+//     <div id="mc_embed_signup_scroll">
+// 	<label for="mce-EMAIL">Subscribe</label>
+// 	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+//     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+//     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0bfa27c5341eacbfd10d85a43_05cac296b0" tabindex="-1" value=""></div>
+//     <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+//     </div>
+// </form>
+// </div>
+
+// <!--End mc_embed_signup--></div>
