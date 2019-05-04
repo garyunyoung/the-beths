cd68c5263061e8c0230f350ab5bbd124b8ca69de
Garyun Young <garyunyoung@gmail.com>
2019-05-04 14:28:39 +0100

    Allow consent banner functionality

--- src/ConsentBanner.js
+++ src/ConsentBanner.js
@@ -2,11 +2,30 @@ import React from "react";
 import "./ConsentBanner.scss";
 
 export default class ConsentBanner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: true
+    };
+  }
+
+  close() {
+    this.setState({ isOpen: false });
+  }
+
+  accept() {
+    this.setState({ isOpen: false });
+  }
+
   render() {
+    if (!this.state.isOpen) {
+      return null;
+    }
     return (
       <div className="banner">
+        <button onClick={() => this.close()}>x</button>
         <p>this is the banner for cookie consent</p>
-        <button>accept</button>
+        <button onClick={() => this.accept()}>accept</button>
       </div>
     );
   }
