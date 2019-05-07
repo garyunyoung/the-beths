04502c1a5d4069691b7d7cc120ba163935d15e3c
Garyun Young <garyunyoung@gmail.com>
2019-05-07 16:42:24 +0100

    Add React lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import cloudinary from "cloudinary-core";
 import { docCookies } from "./cookies";
 import Bar from "./Bar";
@@ -8,10 +8,11 @@ import ConsentBanner from "./ConsentBanner";
 import Header from "./Header";
 import Merch from "./Merch";
 import Watch from "./Watch";
-import Game from "./Game";
 import Tour from "./Tour";
 import Contact from "./Contact";
 import "./App.scss";
+
+const Game = lazy(() => import("./Game"));
 
 const cookieName = "beths-EU-consent";
 
@@ -53,46 +54,49 @@ export default class App extends React.Component {
   }
 
   render() {
-    
     let cl = cloudinary.Cloudinary.new({ cloud_name: "garyou" });
     cl.responsive();
 
     return (
-      <React.Fragment>
-        <BarDesktop toggleGame={() => this.toggleGame()} />
-        <Bar toggleGame={() => this.toggleGame()} />
-        <section className="page page--home">
-          <Home />
-          <ConsentBanner
-            consent={this.state.hasConsent}
-            loadAnalytics={() => this.loadAnalytics()}
-            allowTracking={() => this.loadAnalytics()}
-          />
-        </section>
-        <section id="merch" className="page page--merch">
-          <Header header="merch" />
-          <Merch />
-        </section>
-        <section id="watch" className="page page--watch">
-          <Header header="watch" />
-          <Watch />
-        </section>
-        <section id="game" className="page page--game">
-          <Header header="game" open={this.state.gameIsOpen} />
-          <Game open={this.state.gameIsOpen} />
-        </section>
-        <section id="tour" className="page page--tour">
-          <Header header="tour" />
-          <Tour
-            consent={this.state.hasConsent}
-            allowTracking={() => this.loadAnalytics()}
-          />
-        </section>
-        <section id="contact" className="page page--contact">
-          <Header header="contact" />
-          <Contact />
-        </section>
-      </React.Fragment>
+      <Suspense fallback={<div>loading...</div>}>
+        <React.Fragment>
+          <BarDesktop toggleGame={() => this.toggleGame()} />
+          <Bar toggleGame={() => this.toggleGame()} />
+          <section className="page page--home">
+            <Home />
+            <ConsentBanner
+              consent={this.state.hasConsent}
+              loadAnalytics={() => this.loadAnalytics()}
+              allowTracking={() => this.loadAnalytics()}
+            />
+          </section>
+          <section id="merch" className="page page--merch">
+            <Header header="merch" />
+            <Merch />
+          </section>
+          <section id="watch" className="page page--watch">
+            <Header header="watch" />
+            <Watch />
+          </section>
+          <section id="game" className="page page--game">
+            <Suspense fallback={<div>loading...</div>}>
+              <Header header="game" open={this.state.gameIsOpen} />
+              <Game open={this.state.gameIsOpen} />
+            </Suspense>
+          </section>
+          <section id="tour" className="page page--tour">
+            <Header header="tour" />
+            <Tour
+              consent={this.state.hasConsent}
+              allowTracking={() => this.loadAnalytics()}
+            />
+          </section>
+          <section id="contact" className="page page--contact">
+            <Header header="contact" />
+            <Contact />
+          </section>
+        </React.Fragment>
+      </Suspense>
     );
   }
 }
