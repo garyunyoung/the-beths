ca75b9ad32850eaef1bcb3900d3f1b9910e3edb3
Garyun Young <garyunyoung@gmail.com>
2019-05-14 18:16:51 +0100

    WIP

--- components/Socials.js
+++ components/Socials.js
@@ -43,3 +43,68 @@ export default function Socials(props) {
     </section>
   );
 }
+
+
+// <section className={`socials ${props.className}`}>
+//       <a
+//         href="https://open.spotify.com/artist/7DjwIxbe8kpw4pqnzAMoin"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         <Icon
+//           name="spotify"
+//           width={props.width}
+//           fill={props.color}
+//           className="socials__icon socials__icon--spotify"
+//         />
+//       </a>
+//       <a
+//         href="https://www.facebook.com/thebethsnz/"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         <Icon
+//           name="facebook"
+//           width={props.width}
+//           fill={props.color}
+//           className="socials__icon socials__icon--facebook"
+//         />
+//       </a>
+//       <a
+//         href="https://www.instagram.com/lizstokedstokes/"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         <Icon
+//           name="instagram"
+//           width={props.width}
+//           fill={props.color}
+//           className="socials__icon socials__icon--instagram"
+//         />
+//       </a>
+//       <a
+//         href="https://twitter.com/lizstokedstokes"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         <Icon
+//           name="twitter"
+//           width={props.width}
+//           fill={props.color}
+//           className="socials__icon socials__icon--twitter"
+//         />
+//       </a>
+//       <a
+//         href="https://thebethsnz.bandcamp.com/music"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         <Icon
+//           name="bandcamp"
+//           width={props.width}
+//           fill={props.color}
+//           className="socials__icon socials__icon--bandcamp"
+//         />
+//       </a>
+//       <style jsx>{scss}</style>
+//     </section>
