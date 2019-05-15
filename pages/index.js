9a472d6d8b554f062f99d41f2af6fe4c4c4a5b1e
Garyun Young <garyunyoung@gmail.com>
2019-05-15 14:07:16 +0100

    Remove Font Awesome

--- pages/index.js
+++ pages/index.js
@@ -17,14 +17,6 @@ export default function IndexPage() {
           as="image"
         />
 
-        {/* <!-- Font Awesome --> */}
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
-          crossOrigin="anonymous"
-        />
-
         {/* <!--Font --> */}
         <link
           href="https://fonts.googleapis.com/css?family=Gaegu:300,400,700"
