0d6e3d2a0295af073c0a5b4ffe081e3e4b069936
Garyun Young <garyunyoung@gmail.com>
2020-04-04 16:19:00 +1300

    adjust background

--- components/Home.js
+++ components/Home.js
@@ -3,12 +3,17 @@ import CTAButton from "./CTAButton";
 import Socials from "./_socials";
 
 import MailingList from "./MailingList";
-import { jumpRopeGazersHandwrittenTitle, newAlbumText, cpY } from "./cloudinary";
+import {
+  jumpRopeGazersHandwrittenTitle,
+  newAlbumText,
+  cpY
+} from "./cloudinary";
 import scss from "../styles/Home.scss";
 
 export default function Home() {
   return (
     <section className="home">
+
       <div className="home__content">
         <div className="home__wrapper">
           <img
@@ -42,7 +47,12 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <Socials className="home__socials" width="24" color={"black"} />
+      <div className="home__test-menu">
+        {/* <p className="home__test">music   video store tour contact</p> */}
+        <Socials className="home__test-socials" width="18" color={"black"} />
+        <p className="home__test-mailing-list">mailing list       garyunyoung@gmail.com</p>
+      </div>
+      {/* <Socials className="home__socials" width="24" color={"black"} /> */}
 
       {/* Mailing List */}
       <style jsx>{scss}</style>
