31f54f6c99182ba6a8ff6f1c0041414b779a2a15
Garyun Young <garyun@stitched.co.uk>
2021-01-27 09:50:56 +1300

    Update contact details

--- components/Contact.js
+++ components/Contact.js
@@ -8,36 +8,10 @@ export default function Contact() {
       className="contact"
       // style={{ backgroundImage: "url(" + contact + ")" }}
     >
-      <p className="contact__text">general manager</p>
-      <a className="contact__link" href="mailto:jacob@monstermanagement.com.au">
-        jacob@monstermanagement.com.au
-      </a>
-      <br />
-      <p className="contact__text">booking agent</p>
-      <p className="contact__text">
-        nz:{" "}
-        <a className="contact__link" href="mailto:anthony@onehundredpercent.nz">
-          anthony@onehundredpercent.nz
+      <p className="contact__text">management</p>
+        <a className="contact__link" href="mailto:info@monstermanagement.com.au">
+          info@monstermanagement.com.au
         </a>
-      </p>
-      <p className="contact__text">
-        aus:{" "}
-        <a className="contact__link" href="mailto:casey@selectmusic.com.au">
-          casey@selectmusic.com.au
-        </a>
-      </p>
-      <p className="contact__text">
-        us/can:{" "}
-        <a className="contact__link" href="mailto:brian@madison-house.com">
-          brian@madison-house.com
-        </a>
-      </p>
-      <p className="contact__text">
-        eu/uk:{" "}
-        <a className="contact__link" href="mailto:joren@hometown-talent.com">
-          joren@hometown-talent.com
-        </a>
-      </p>
       <br />
       <p className="contact__text">record label</p>
       <a
