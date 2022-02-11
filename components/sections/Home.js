f079b6dc2e8247d598c4be1d297b354231a461e4
Garyun Young <garyunyoung@gmail.com>
2022-02-11 15:21:03 +1300

    Add background image

--- components/sections/Home.js
+++ components/sections/Home.js
@@ -4,11 +4,10 @@ import scss from '../../styles/Home.scss';
 export default function Home() {
   return (
     <section className='home'>
-      <div className='home__background-image-wrapper'>
-        <img className='home__background-image home__background-image' src='https://res.cloudinary.com/garyou/image/upload/w_auto,c_scale,q_auto,dpr_auto,f_auto,q_auto:good/v1629344430/the-beths/town-hall-live-2020/Beths_Live_Amazon_Artwork_Poster_Out_Now_zsacjy.jpg' alt="The Beths playing onstage at Auckland's Town Hall" />
-        {/* <img className='home__background-image home__background-image--desktop' src='https://res.cloudinary.com/garyou/image/upload/w_auto,c_scale,q_auto,dpr_auto,f_auto,q_auto:good/v1629343962/the-beths/town-hall-live-2020/Beths_Live_Youtube_2048x1152_1_utnqgy.jpg' alt="The Beths playing onstage at Auckland's Town Hall" /> */}
-        <img className='home__background-image home__background-image--desktop' src='https://res.cloudinary.com/garyou/image/upload/w_auto,c_scale,q_auto,dpr_auto,f_auto,q_auto:good/v1634616653/the-beths/town-hall-live-2020/Beths_Live_Youtube_2048x1152_Out_Now_tan7np.jpg' alt="The Beths playing onstage at Auckland's Town Hall" />
-      </div>
+      <div className='home__background-image-wrapper'
+        style={{ backgroundImage: `url('https://res.cloudinary.com/garyou/image/upload/w_auto,c_scale,q_auto,dpr_auto,f_auto,q_auto:good/v1644545813/the-beths/a-real-thing/a-real-thing-background-mobile-edited_al5vua.png')` }}></div>
+      <div className='home__background-image-wrapper home__background-image-wrapper--desktop'
+        style={{ backgroundImage: `url('https://res.cloudinary.com/garyou/image/upload/w_auto,c_scale,q_auto,dpr_auto,f_auto,q_auto:good/v1644542011/the-beths/a-real-thing/a-real-thing-background-desktop-edited_wcvq0t.png')` }}></div>
       <div className='home__cta-links '>
         <a
           className='home__cta-link'
@@ -28,6 +27,6 @@ export default function Home() {
         </a>
       </div>
       <style jsx>{scss}</style>
-    </section>
+    </section >
   );
 }
