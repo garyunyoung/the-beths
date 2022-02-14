54abf5bbda8bfed62dcc304cd3ad4ed16db4d7d4
Garyun Young <garyunyoung@gmail.com>
2022-02-14 18:28:43 +1300

    Replace home section cta with external link component

--- components/sections/Home.js
+++ components/sections/Home.js
@@ -1,31 +1,38 @@
 import React from 'react';
+import ExternalLink from '../partials/Links';
 
 import scss from '../../styles/Home.scss';
+
+const aRealThingSingleArt = 'https://res.cloudinary.com/garyou/image/upload/w_auto,c_scale,q_auto,dpr_auto,f_auto,q_auto:good/v1644812004/the-beths/a-real-thing/CAKD80_TheBeths_ARealThing_pnebmg.tiff'
+
+const callToActions = [
+  {
+    href: 'https://youtu.be/vd2Rps0cMdo',
+    text: 'WATCH VIDEO'
+  },
+  {
+
+    href: 'https://found.ee/beths_arealthing',
+    text: 'BUY / STREAM SINGLE'
+  }
+]
 
 export default function Home() {
   return (
     <section className='home'>
       <div className='home__single-art'
         style={{
-          backgroundImage: `url('https://res.cloudinary.com/garyou/image/upload/w_auto,c_scale,q_auto,dpr_auto,f_auto,q_auto:good/v1644812004/the-beths/a-real-thing/CAKD80_TheBeths_ARealThing_pnebmg.tiff')`
+          backgroundImage: `url(${aRealThingSingleArt})`
         }}></div>
       <div className='home__cta-links '>
-        <a
-          className='home__cta-link'
-          href='https://youtu.be/vd2Rps0cMdo'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          WATCH VIDEO
-        </a>
-        <a
-          className='home__cta-link'
-          href='https://found.ee/beths_arealthing'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          BUY / STREAM SINGLE
-        </a>
+        {callToActions.map(({ href, text }, index) => (
+          <ExternalLink
+            className='home__cta-link'
+            key={index}
+            href={href}>
+            {text}
+          </ExternalLink>
+        ))}
       </div>
       <style jsx>{scss}</style>
     </section >
