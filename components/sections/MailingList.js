f8b0cf53feee9983332ce132a8c6d02b8a01ea71
Garyun Young <garyunyoung@gmail.com>
2022-02-15 17:18:29 +1300

    Update mailing list to functional component

--- components/sections/MailingList.js
+++ components/sections/MailingList.js
@@ -1,77 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import scss from "../../styles/MailingList.scss";
 
-export default class MailingList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      emailValue: "",
-      isOpen: false
-    };
-  }
+function MailingList() {
+  const [emailValue, setEmailValue] = useState('')
+  const [isOpen, setisOpen] = useState(false)
 
-  render() {
-    return (
-      <section className="mailing-list-banner">
-        <div className="mailing-list-banner__wrapper">
-          <div
-            className='mailing-list-banner__cta-link'
-            onClick={e => {
-              this.setState({ isOpen: !this.state.isOpen });
-            }}
-
+  return (
+    <section className="mailing-list-banner">
+      <div className="mailing-list-banner__wrapper">
+        <div
+          className='mailing-list-banner__cta-link'
+          onClick={() => setisOpen(!isOpen)}
+        >
+          JOIN MAILING LIST
+        </div>
+        <div id="mc_embed_signup" className={`mailchimp ${`${isOpen ? 'is-open' : ''}`}`}>
+          <form
+            action="https://thebeths.us20.list-manage.com/subscribe/post?u=0bfa27c5341eacbfd10d85a43&amp;id=05cac296b0"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            className="validate"
+            target="_blank"
           >
-            JOIN MAILING LIST
-          </div>
-          <div id="mc_embed_signup" className={`mailchimp ${`${this.state.isOpen ? 'is-open' : ''}`}`}>
-            <form
-              action="https://thebeths.us20.list-manage.com/subscribe/post?u=0bfa27c5341eacbfd10d85a43&amp;id=05cac296b0"
-              method="post"
-              id="mc-embedded-subscribe-form"
-              name="mc-embedded-subscribe-form"
-              className="validate"
-              target="_blank"
-            >
-              <div id="mc_embed_signup_scroll" className="mailing-list-banner__form-content">
-                <label htmlFor="mce-EMAIL" className="mailing-list-banner__email-input-label">join our mailing list!</label>
-                <div className="mailing-list-banner__input-wrapper input-wrapper">
+            <div id="mc_embed_signup_scroll" className="mailing-list-banner__form-content">
+              <label htmlFor="mce-EMAIL" className="mailing-list-banner__email-input-label">join our mailing list!</label>
+              <div className="mailing-list-banner__input-wrapper input-wrapper">
+                <input
+                  type="email"
+                  value={emailValue}
+                  onChange={(e) => setEmailValue(e.target.value)}
+                  name="EMAIL"
+                  className="mailing-list-banner__email-input-field email"
+                  id="mce-EMAIL"
+                  placeholder="email address"
+                  required
+                />
+                <div className="mailing-list-banner__aria aria" aria-hidden="true">
                   <input
-                    type="email"
-                    value={this.state.emailValue}
-                    onChange={e => {
-                      this.setState({ emailValue: e.target.value });
-                    }}
-                    name="EMAIL"
-                    className="mailing-list-banner__email-input-field email"
-                    id="mce-EMAIL"
-                    placeholder="email address"
-                    required
+                    type="text"
+                    name="b_0bfa27c5341eacbfd10d85a43_05cac296b0"
+                    tabIndex="-1"
+                    value=""
+                    readOnly
                   />
-                  <div className="mailing-list-banner__aria aria" aria-hidden="true">
-                    <input
-                      type="text"
-                      name="b_0bfa27c5341eacbfd10d85a43_05cac296b0"
-                      tabIndex="-1"
-                      value=""
-                      readOnly
-                    />
-                  </div>
-                  <div className="clear">
-                    <input
-                      className="mailing-list-banner__submit-button button"
-                      type="submit"
-                      value="subscribe"
-                      name="subscribe"
-                      id="mc-embedded-subscribe"
-                    />
-                  </div>
+                </div>
+                <div className="clear">
+                  <input
+                    className="mailing-list-banner__submit-button button"
+                    type="submit"
+                    value="subscribe"
+                    name="subscribe"
+                    id="mc-embedded-subscribe"
+                  />
                 </div>
               </div>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
-        <style jsx>{scss}</style>
-      </section>
-    );
-  }
+      </div>
+      <style jsx>{scss}</style>
+    </section >
+  );
+
 }
+
+export default MailingList
