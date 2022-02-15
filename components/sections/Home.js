b5df7766cab778246e4d2863bc72442eef9cec52
Garyun Young <garyunyoung@gmail.com>
2022-02-15 16:14:19 +1300

    Add basic style to desktop home page

--- components/sections/Home.js
+++ components/sections/Home.js
@@ -24,23 +24,29 @@ const callToActions = [
 export default function Home() {
   return (
     <section className='home'>
-      <span className='home__single-art-wrapper'>
-        <img
-          className='home__single-art'
-          src={aRealThingSingleArt}
-          alt="Pink squiggles on an yellow orb designed by Phillipa Emery"
-        />
+      <span className='home__top'>
+        <span className='home__left'>
+          <span className='home__single-art-wrapper'>
+            <img
+              className='home__single-art'
+              src={aRealThingSingleArt}
+              alt="Pink squiggles on an yellow orb designed by Phillipa Emery"
+            />
+          </span>
+        </span>
+        <span className='home__right'>
+          <img
+            className='home__text-image'
+            src={aRealThingText}
+            alt="The Beths 'A Real Thing' out now via Carpark Records and Ivy League Records"
+          />
+          <img
+            className='home__record-label-logos'
+            src={recordLabelLogos}
+            alt=""
+          />
+        </span>
       </span>
-      <img
-        className='home__text-image'
-        src={aRealThingText}
-        alt="The Beths 'A Real Thing' out now via Carpark Records and Ivy League Records"
-      />
-      <img
-        className='home__record-label-logos'
-        src={recordLabelLogos}
-        alt=""
-      />
       <div className='home__cta-links'>
         {callToActions.map(({ href, text }, index) => (
           <ExternalLink
