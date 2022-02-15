e30b9415144867d267347a5d7b1b9d69a0b62ce2
Garyun Young <garyunyoung@gmail.com>
2022-02-16 12:41:59 +1300

    Update tour to function component

--- components/sections/Tour.js
+++ components/sections/Tour.js
@@ -1,58 +1,57 @@
-import React from "react";
-import { tourTheBeths } from "../utilities/cloudinary.js";
-import scss from "../../styles/Tour.scss";
+import React, { useEffect, useRef } from "react"
+import { tourTheBeths } from "../utilities/cloudinary.js"
+import scss from "../../styles/Tour.scss"
 
-export default class Tour extends React.Component {
-  loadSongkick() {
-    const script = document.createElement("script");
-    script.src = "//widget.songkick.com/8923484/widget.js";
-    script.async = true;
-    document.body.appendChild(script);
+function Tour({ consent, allowTracking }) {
+  function loadSongkick() {
+    const script = document.createElement("script")
+    script.src = "//widget.songkick.com/8923484/widget.js"
+    script.async = true
+    document.body.appendChild(script)
   }
 
-  componentDidUpdate(prevProps) {
-    if (this.props.consent !== prevProps.consent && this.props.consent) {
-      this.loadSongkick();
+  useEffect(() => {
+    if (consent) {
+      loadSongkick()
     }
-  }
+  }, [consent])
 
-  render() {
-    if (!this.props.consent) {
-      return (
-        <div className="tour-consent-banner">
-          <p className="tour-consent-banner__text">
-            our songkick widget requires analytics to run, accept our cookies
-            to see what shows we have lined up!
-          </p>
-          <button className="tour-consent-banner__accept-button" onClick={() => this.props.allowTracking()}>
-            accept
-          </button>
-          <style jsx>{scss}</style>
+  if (!consent) {
+    return (
+      <div className="tour-consent-banner">
+        <p className="tour-consent-banner__text">
+          Our Songkick widget requires analytics to run, accept our cookies
+          to see what shows we have lined up!
+        </p>
+        <button className="tour-consent-banner__accept-button"
+          onClick={() => allowTracking()}>
+          accept
+        </button>
+        <style jsx>{scss}</style>
+      </div>
+    )
+  } else {
+    return (
+      <section className="tour">
+        <div
+          className="tour__image"
+          style={{ backgroundImage: "url(" + tourTheBeths + ")" }}
+        ></div>
+        <p className="tour__text">come gig!</p>
+        <div id='song-kick' className="song-kick">
+          <a
+            href="https://www.songkick.com/artists/8923484"
+            className="songkick-widget"
+            data-theme="light"
+            data-track-button="on"
+            data-detect-style="true"
+            data-background-color="transparent"
+          />
         </div>
-      );
-    } else {
-      return (
-        <section className="tour">
-          <div
-            className="tour__image"
-            style={{ backgroundImage: "url(" + tourTheBeths + ")" }}
-          ></div>
-          <p className="tour__text">come gig!</p>
-
-
-          <div id='song-kick' className="song-kick">
-            <a
-              href="https://www.songkick.com/artists/8923484"
-              className="songkick-widget"
-              data-theme="light"
-              data-track-button="on"
-              data-detect-style="true"
-              data-background-color="transparent"
-            />
-          </div>
-          <style jsx>{scss}</style>
-        </section>
-      );
-    }
+        <style jsx>{scss}</style>
+      </section>
+    )
   }
 }
+
+export default Tour
