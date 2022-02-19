3d6e6ddc37fe8984c4b9cf0fc414f85c5db54abd
Garyun Young <garyunyoung@gmail.com>
2022-02-19 21:59:49 +1300

    Consolidate game files

--- components/game/Game2048.js
+++ components/game/Game2048.js
@@ -1,8 +1,8 @@
 import React from "react";
-import GameManager from "../game/game_manager";
-import KeyboardInputManager from "../game/keyboard_input_manager";
-import HTMLActuator from "../game/html_actuator";
-import LocalStorageManager from "../game/local_storage_manager";
+import GameManager from "./engine/game_manager";
+import KeyboardInputManager from "./engine/keyboard_input_manager";
+import HTMLActuator from "./engine/html_actuator";
+import LocalStorageManager from "./engine/local_storage_manager";
 import scssA from "../../styles/Game.scss";
 import scssB from "../../styles/_gameHelpers.scss";
 
--- pages/game/index.js
+++ pages/game/index.js
@@ -2,7 +2,7 @@ import dynamic from "next/dynamic";
 
 import Header from "../../components/partials/Header"
 
-const Game2048 = dynamic(import("../../components/partials/Game2048"))
+const Game2048 = dynamic(import("../../components/game/Game2048"))
 
 function Game() {
   return (
