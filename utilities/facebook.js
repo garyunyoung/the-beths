96bd58922a8410d9c6af0b2bc162d24c87ebe939
Garyun Young <garyunyoung@gmail.com>
2022-03-02 09:57:57 +1300

    Consolidate facebook pixels

--- utilities/facebook.js
+++ utilities/facebook.js
@@ -16,21 +16,6 @@ export default function Facebook() {
                 s.parentNode.insertBefore(t,s)}(window, document,'script',
                 'https://connect.facebook.net/en_US/fbevents.js');
                 fbq('init', '701632633715996');
-                fbq('track', 'PageView');`,
-        }}
-      />
-
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `!function(f,b,e,v,n,t,s) {
-                if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-                n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
                 fbq('init', '386188229885916');
                 fbq('track', 'PageView');`,
         }}
